f313f5660ffbef1bda33079dc3d0e73914a55e6f
nikhilka <nikhilka23@gmail.com>
2024-08-09 20:03:23 +0530

    Slight Responsive design corrections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,28 @@ import { Link } from "react-router-dom";
 export default function Header({ listChoice, changeListHandler }) {
 	return (
 		<nav className="navbar">
-			<div className="container-fluid d-flex text-dark justify-content-space-between">
-				<Link to="/" style={{ textDecoration: "none", color: "black" }}>
-					<img
-						src={logo}
-						alt="My Favourite MOvies"
-						width="80"
-						// height="48"
-						className="d-inline-block align-text-top"
-					/>
-				</Link>
-				<h4>Your Favourite Movie DB</h4>
-				<Link to="/" style={{ textDecoration: "none", color: "black" }}>
-					<h1>TMDB</h1>
-				</Link>
+			<div className="container-fluid d-flex text-dark text-center">
+				<div className="row d-flex justify-content-between navbar-content">
+					<div className="col-sm-12 col-md-4 my-1">
+						<Link to="/" style={{ textDecoration: "none", color: "black" }}>
+							<img
+								src={logo}
+								alt="My Favourite MOvies"
+								width="80"
+								// height="48"
+								className="d-inline-block align-text-top"
+							/>
+						</Link>
+					</div>
+					<div className="col-sm-12 col-md-4 my-1">
+						<h4>Your Favourite Movie DB</h4>
+					</div>
+					<div className="col-sm-12 col-md-4 my-1">
+						<Link to="/" style={{ textDecoration: "none", color: "black" }}>
+							<h1>TMDB</h1>
+						</Link>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -12,28 +12,34 @@ export default function MovieList({
 }) {
 	return (
 		<article className="mt-4">
-			<section className="heading-list">
-				<h2 className="text-center mb-4">{list}</h2>
-				<div className="list-chooser d-flex ">
-					<select
-						id="listChooser"
-						name="listChooser"
-						className="form-select"
-						aria-label="Default select example"
-						value={listChoice}
-						onChange={(evt) => {
-							changeListHandler(evt.target.value);
-						}}
-					>
-						{" "}
-						<option value="Default" disabled>
-							Choose a List
-						</option>
-						<option value="POPULAR">POPULAR</option>
-						<option value="NOW PLAYING">NOW PLAYING</option>
-						<option value="TOP RATED">TOP RATED</option>
-						<option value="UPCOMING">UPCOMING</option>
-					</select>
+			<section className="heading-list align-items-center d-flex">
+				<div className="row heading-list-row d-flex justify-content-between align-items-center">
+					<div className="col-lg-6 col-md-12 justify-content-center mx-auto">
+						<h2 className="text-center mb-4">{list}</h2>
+					</div>
+					<div className="col-lg-6 col-md-12 d-flex justify-content-center mx-auto">
+						<div className="list-chooser d-flex text-center ">
+							<select
+								id="listChooser"
+								name="listChooser"
+								className="form-select"
+								aria-label="Default select example"
+								value={listChoice}
+								onChange={(evt) => {
+									changeListHandler(evt.target.value);
+								}}
+							>
+								{" "}
+								<option value="Default" disabled>
+									Choose a List
+								</option>
+								<option value="POPULAR">POPULAR</option>
+								<option value="NOW PLAYING">NOW PLAYING</option>
+								<option value="TOP RATED">TOP RATED</option>
+								<option value="UPCOMING">UPCOMING</option>
+							</select>
+						</div>
+					</div>
 				</div>
 			</section>
 			<section className="movie-list d-flex flex-wrap justify-content-center">
